180eaea929b1ba25bfff5c1221b262a0f314d513
Perumallpardhiv <LIT2021001@iiitl.ac.in>
2023-02-10 02:19:07 +0530

    Chatgpt start

---
+++ lib/chatscreen.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class chatscreen extends StatefulWidget {
+  const chatscreen({super.key});
+
+  @override
+  State<chatscreen> createState() => _chatscreenState();
+}
+
+class _chatscreenState extends State<chatscreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      
+    );
+  }
+}
